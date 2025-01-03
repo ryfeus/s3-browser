f8e70b4adeadd006fa4056010d2b9a3c319512f5
David Days <david.days@maplarge.com>
2025-01-03 16:11:04 -0500

    Create parent links back to root directory

--- generate_index.py
+++ generate_index.py
@@ -31,6 +31,13 @@ def generateIndexFile(strBucket,strPrefix,strIndexFile,vecFiles,vecFolders,strTe
 
     tagKeysList.append(generateHeader(soup, strBucket, strPrefix))
 
+    if (strPrefix.count('/') > 0):
+        strAncestors = strPrefix.split('/')[:-2]
+        if len(strAncestors) > 0:
+            tagKeysList.append(generateElement(soup, True, '..', '/' + '/'.join(strAncestors) + '/index.html'))
+        else:
+            tagKeysList.append(generateElement(soup, True, '..', '/index.html'))
+
     for strFolder in vecFolders:
         strFolderLast = strFolder.split('/')[-2]
         tagKeysList.append(generateElement(soup, True, strFolderLast, '/' + strFolder + 'index.html'))
